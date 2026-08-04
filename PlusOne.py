a22616c8b3cd991b70617029fde857ec30ad9cc2
Lucas Alves <lcalvesoliveira1@gmail.com>
2026-08-04 14:42:18 -0300

    Add problem 66. Plus One resolution from LeetCode

---
+++ PlusOne.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if digits[-1] < 9:
+            digits[-1] +=1
+            return digits
+        
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+            
+        digits.insert(0,1)            
+        return digits
+
+        
+
+a = Solution()
+print(a.plusOne([9,9,9]))
